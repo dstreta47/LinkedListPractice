1e3752606e83ae6b3d991f1ced22c859b82a4555
Amit Rana <43043638+dstreta47@users.noreply.github.com>
2021-08-25 19:00:03 -0500

    id 556333

---
+++ CircularLL.py
@@ -0,0 +1,21 @@
+Class Node:
+  #constructor
+  def__init__(self):
+    self.data = None
+    self.next = None
+  #Setter  
+  def set_data(self,data):
+    self.data = data
+  #getter  
+  def getData(self):
+    return self.data
+  #set next
+  def setNext(self):
+    self.next = next
+  #get next  
+  def getNext(self):
+    return self.next
+  #has next
+  def hasNext(self):
+    return self.next!=None
+  
